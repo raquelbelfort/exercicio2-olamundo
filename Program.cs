3e877e28eec3c36718335d48f267153878b1dfbb
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-02-28 20:36:07 -0300

    Adição de resolução e enunciado.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio2_olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Raquel!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # exercicio2-olamundo
 Resolução do exercício de saída via console - OlaMundo
+
+Enunciado:
+ "Faça um programa que exiba Olá usuário, eu me chamo Ermogenes! (utilize o seu nome)."
